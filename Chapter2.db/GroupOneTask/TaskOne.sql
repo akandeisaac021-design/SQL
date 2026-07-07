3a3585addc4078dd3d41ca85f63282dd88e99b4c
akandeisaac021-design <akandeisaac021@gmail.com>
2026-07-07 09:55:11 +0100

    group 2

--- Chapter2.db/GroupOneTask/TaskOne.sql
+++ Chapter2.db/GroupOneTask/TaskOne.sql
@@ -1,25 +1,30 @@
-SELECT * FROM CUSTOMER;
-SELECT * FROM INVOICE;
-SELECT * FROM INVOICE_ITEM;
+SELECT * FROM CUSTOMER, INVOICE, INVOICE_ITEM;
 
+SELECT FirstName, LastName, Phone FROM CUSTOMER;
 
-SELECT LastName, FirstName, Phone 
-FROM CUSTOMER;
+SELECT FirstName, LastName, Phone FROM CUSTOMER where FirstName = "Nikki";
 
+SELECT FirstName, LastName, Phone, DateIn, DateOut
+FROM CUSTOMER, INVOICE
+WHERE TotalAmount > 100.00;
 
-SELECT LastName, FirstName, Phone 
-FROM CUSTOMER 
-WHERE FirstName = 'Nikki';
-
-
-
-SELECT c.LastName, c.FirstName, c.Phone, i.DateIn, i.DateOut
-FROM CUSTOMER c
-JOIN INVOICE i ON c.CustomerID = i.CustomerID
-WHERE i.TotalAmount > 100.00;
+SELECT FirstName, LastName, Phone FROM CUSTOMER where FirstName LIKE "B%"; 
 
 
 
-SELECT LastName, FirstName, Phone 
-FROM CUSTOMER 
-WHERE FirstName LIKE 'B%';
+SELECT FirstName, LastName, Phone FROM CUSTOMER where LastName LIKE "%cat%";
+
+SELECT FirstName, LastName, Phone FROM CUSTOMER where phone LIKE "_23%"; 
+
+SELECT max(TotalAmount), min(TotalAmount) from INVOICE;
+SELECT avg(TotalAmount) from INVOICE;
+SELECT count(*) from CUSTOMER;
+
+SELECT * FROM CUSTOMER ORDER BY FirstName
+
+SELECT      LastName, 
+            FirstName, 
+            COUNT(*) AS NumberOfCustomers
+FROM        CUSTOMER
+GROUP BY    LastName, FirstName;
+
---
+++ chapter3.db/group2.sql
@@ -0,0 +1,7 @@
+create table Project(
+ProjectName varchar(50) NOT NULL,
+EmployeeName varchar(50) NOT NULL,
+
+primary key (ProjectName)
+-- ProjectName -> EmployeeName,
+);
